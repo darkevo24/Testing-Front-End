e1b6e6f8f0853af92d64c13f84fa228c1af76f1f
Brijesh Bhakta <brijeshbhakta30@gmail.com>
2021-10-27 22:11:01 +0530

    refactor: updated cookies helper with more features

---
+++ src/utils/cookie.js
@@ -0,0 +1,27 @@
+import Cookies from 'js-cookie';
+import { safeParse, safeStringify } from './helper';
+import bn from './bemNames';
+
+const bem = bn('cookie');
+
+export const cookieKeys = {
+  token: bem.e('token'),
+};
+
+export const getCookieByName = (name) => {
+  const match = Cookies.get(name);
+  if (match) {
+    return safeParse(match);
+  }
+  return null;
+};
+
+export const setCookie = (name, value) => {
+  Cookies.set(name, safeStringify(value));
+};
+
+export const removeAllCookie = () => {
+  Object.keys(Cookies.get()).forEach(function (cookieName) {
+    Cookies.remove(cookieName);
+  });
+};

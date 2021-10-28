8945f84bdd34f162ccc820bb849edc911dc8cecd
Brijesh Bhakta <brijeshbhakta30@gmail.com>
2021-10-28 20:35:46 +0530

    feat: allow input field to have form group and label

--- src/components/Input.js
+++ src/components/Input.js
@@ -1,18 +1,20 @@
 import Form from 'react-bootstrap/Form';
 import { Controller } from 'react-hook-form';
 
-export const Input = ({ name, control, rules, error, ...rest }) => {
-  return (
+export const Input = ({ name, control, rules, error, label, group, groupClass = 'mb-3', ...rest }) => {
+  const inputNode = (
     <>
+      {label && <Form.Label>{label}</Form.Label>}
       <Controller
         name={name}
         control={control}
         rules={rules}
-        render={({ field }) => <Form.Control {...rest} {...field} />}
+        render={({ field }) => <Form.Control maxLength={200} {...rest} {...field} />}
       />
       <div className="sdp-error">{error}</div>
     </>
   );
+  return group ? <Form.Group className={groupClass}>{inputNode}</Form.Group> : inputNode;
 };
 
 export default Input;

23fe6e8c053ce4a6e32ef6143e30b2c1aa39ca6f
Eka DDM <eka.p@deltadatamandiri.com>
2021-11-18 10:17:17 +0700

    fix: 🐛 public tentang layout

--- src/containers/TentangSDI/index.js
+++ src/containers/TentangSDI/index.js
@@ -3,37 +3,15 @@ import Col from 'react-bootstrap/Col';
 import Row from 'react-bootstrap/Row';
 
 import Logo from 'assets/logo-satu-data-id.png';
-import './tentangsdi.scss';
 
 import ContactUs from './form.js';
 import TentangProfile from './profile.js';
+import bn from 'utils/bemNames';
+import cx from 'classnames';
+
+const bem = bn('tentang');
 
 const TentangSDI = () => {
-  const OperationalItem = styled.div`
-    background: #ffffff;
-    border: 2px solid #e1e2ea;
-    box-sizing: border-box;
-    border-radius: 4px;
-    font-size: 15px;
-    line-height: 18px;
-    align-items: center;
-    text-align: center;
-    color: #2d2627;
-    width: 223.2px;
-    height: 112px;
-    margin-right: 16px;
-    margin-bottom: 16px;
-    padding: 0 8px;
-    display: inline-grid;
-  `;
-
-  const OperationalTop = styled.div`
-    width: 48px;
-    height: 2px;
-    margin-left: 16px;
-    background: #ff0000;
-  `;
-
   let strukturOrganisasi = [
     {
       name: 'Oktorialdi, Ph.D',
@@ -54,6 +32,7 @@ const TentangSDI = () => {
       level: 2,
     },
   ];
+  
   let arrayLevel = [];
   for (var i = 0; i < strukturOrganisasi.length; i++) {
     if (arrayLevel.indexOf(strukturOrganisasi[i].level) === -1) {
@@ -71,8 +50,8 @@ const TentangSDI = () => {
 
   return (
     <div>
-      <div className="tentang-container">
-        <div className="tentang-logo">
+      <div className={bem.e('container')}>
+        <div className={bem.e('logo')}>
           <img src={Logo} alt="" />
         </div>
         <iframe
@@ -84,8 +63,8 @@ const TentangSDI = () => {
           allow="accelerometer; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
           allowfullscreen></iframe>
         <div className="mt-5">
-          <div className="tentang-title mb-3">Tentang SDI</div>
-          <p className="tentang-desc">
+          <div className={cx(bem.e('title'), 'mb-3')}>Tentang SDI</div>
+          <p className={bem.e('description')}>
             Satu Data Indonesia (SDI) merupakan kebijakan tata kelola data pemerintah yang bertujuan untuk menciptakan data
             berkualitas, mudah diakses, dan dapat dibagipakaikan antar Instansi Pusat serta Daerah. Kebijakan ini tertuang
             dalam Peraturan Presiden no. 39 tahun 2019 tentang Satu Data Indonesia. Melalu SDI, seluruh data pemerintah dan
@@ -101,17 +80,17 @@ const TentangSDI = () => {
         <div className="mt-100">
           <Row className="align-items-center mb-5">
             <Col>
-              <div style={{ background: '#FF0000', height: '4px' }}></div>
+              <div className={bem.e('red-line')}></div>
             </Col>
             <Col>
-              <div className="tentang-title text-center">Struktur Organisasi</div>
+              <div className={cx(bem.e('title'), 'text-center')}>Struktur Organisasi</div>
             </Col>
             <Col>
-              <div style={{ background: '#FF0000', height: '4px' }}></div>
+              <div className={bem.e('red-line')}></div>
             </Col>
           </Row>
           {arrayLevel.map((level) => (
-            <div style={{ textAlign: 'center' }}>
+            <div className="text-center">
               {strukturOrganisasi
                 .filter((item) => {
                   return item.level === level;
@@ -125,20 +104,20 @@ const TentangSDI = () => {
         <div className="mt-5">
           <Row className="align-items-center mb-5">
             <Col xs={3}>
-              <div className="tentang-title-sm">Bidang Operational</div>
+              <div className={bem.e('title-sm')}>Bidang Operational</div>
             </Col>
             <Col>
-              <div style={{ background: '#E1E2EA', height: '2px' }}></div>
+              <div className={bem.e(('grey-line'))}></div>
             </Col>
           </Row>
-          <Row className="align-items-center ">
+          <div className="d-flex flex-wrap">
             {bidangOperasional.map((item, key) => (
-              <Col xs={3}>
-                <OperationalTop />
-                <OperationalItem key={key}>{item}</OperationalItem>
-              </Col>
+              <div key={key}>
+                <div className={bem.e('card-top')}></div>
+                <div className={bem.e('card')}>{item}</div>
+              </div>
             ))}
-          </Row>
+          </div>
         </div>
       </div>
       <ContactUs />

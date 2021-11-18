f61268f53230fc7fd2edcfb25e30cdda6d1bccbe
Iwan Kurnia <iwan@deltadatamandiri.com>
2021-11-18 19:05:24 +0700

    feat: ✨About Us List

--- src/containers/App/CMSRoutes.js
+++ src/containers/App/CMSRoutes.js
@@ -30,10 +30,13 @@ function CMSRoutes() {
         <PrivateRoute exact path="/cms/komunitas-ahli" component={KomunitasAhliPage} />
         <PrivateRoute exact path="/cms/dashboard" component={DashboardPage} />
         <PrivateRoute exact path="/cms/about-us" component={AboutUsPage} />
+<<<<<<< HEAD
         <PrivateRoute exact path="/cms/about-us/edit" component={AboutUsEditPage} />
         <PrivateRoute exact path="/cms/struktur" component={StrukturOrganisasiPage} />
         <PrivateRoute exact path="/cms/struktur-baru" component={StrukturBaruPage} />
         <PrivateRoute exact path="/cms/struktur-detail/:id" component={StrukturDetailPage} />
+=======
+>>>>>>> feat: ✨About Us List
         <PrivateRoute exact path="/cms/about-us/edit/:id?" component={AboutUsEditPage} />
         <PrivateRoute exact path="/cms/berita" component={BeritaPage} />
         <PrivateRoute exact path="/cms/berita-baru" component={BeritaBaruPage} />

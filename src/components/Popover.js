d4dd02cbca4d4223849bb230c8c7cb9c1cdeb0d7
Brijesh Bhakta <brijeshbhakta30@gmail.com>
2021-11-24 14:17:41 +0530

    feat: ✨ popover component with designs

---
+++ src/components/Popover.js
@@ -0,0 +1,22 @@
+import RBPopover from 'react-bootstrap/Popover';
+import OverlayTrigger from 'react-bootstrap/OverlayTrigger';
+import cx from 'classnames';
+import bn from 'utils/bemNames';
+
+const bem = bn('popover');
+
+const Popover = ({ children, className, header, trigger, triggerOn = 'click', placement = 'bottom', ...rest }) => {
+  const overlay = (
+    <RBPopover id="pppppopver" className={cx(bem.b(), className)} {...rest}>
+      {header && <RBPopover.Header className={bem.e('header')}>{header}</RBPopover.Header>}
+      <RBPopover.Body className={bem.e('body')}>{children}</RBPopover.Body>
+    </RBPopover>
+  );
+  return (
+    <OverlayTrigger trigger={triggerOn} overlay={overlay} placement={placement} {...rest}>
+      {trigger}
+    </OverlayTrigger>
+  );
+};
+
+export default Popover;

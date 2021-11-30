b3c7bf4617ee24ea39835c4b7f021dfe36df5128
Brijesh Bhakta <brijeshbhakta30@gmail.com>
2021-11-30 16:44:53 +0530

    feat: ✨ added more options in cms user navigation

--- src/containers/Header/CMSHeader.js
+++ src/containers/Header/CMSHeader.js
@@ -1,6 +1,7 @@
 import { useCallback } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { useHistory } from 'react-router-dom';
+import { useTranslation } from 'react-i18next';
 import NavDropdown from 'react-bootstrap/NavDropdown';
 import { logout, userSelector } from 'containers/Login/reducer';
 import Logo from 'assets/logo-satu.jpg';
@@ -8,6 +9,7 @@ import Logo from 'assets/logo-satu.jpg';
 export const CMSHeader = () => {
   const history = useHistory();
   const user = useSelector(userSelector);
+  const { t } = useTranslation();
 
   const goTo = (params) => () => history.push(params);
 
@@ -28,7 +30,9 @@ export const CMSHeader = () => {
           </li>
           <li className="d-flex justify-content-end flex-row align-items-center my-19 bg-gray-lighter">
             <NavDropdown title={user?.name || 'Achmad Adam'} id="user-nav-dropdown" className="user-nav h-100">
-              <NavDropdown.Item onClick={handleLogout}>Sign Out</NavDropdown.Item>
+              <NavDropdown.Item>{t('header.userNav.changePassword')}</NavDropdown.Item>
+              <NavDropdown.Item>{t('header.userNav.privacyPolicy')}</NavDropdown.Item>
+              <NavDropdown.Item onClick={handleLogout}>{t('header.userNav.signOut')}</NavDropdown.Item>
             </NavDropdown>
           </li>
         </ul>

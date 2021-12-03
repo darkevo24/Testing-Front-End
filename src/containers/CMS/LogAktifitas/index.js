ddf05c838f9ff0e09e43c9a5101548a3cbc64115
adamnurdin03 <adamnurdin03@gmail.com>
2021-12-03 21:39:40 +0700

    feat: ✨add header log-activity

--- src/containers/CMS/LogAktifitas/index.js
+++ src/containers/CMS/LogAktifitas/index.js
@@ -1,9 +1,28 @@
+import Button from 'react-bootstrap/Button';
 import { useHistory, useLocation } from 'react-router-dom';
-import { CMSTable } from 'components';
+import { useForm } from 'react-hook-form';
+import { yupResolver } from '@hookform/resolvers/yup';
+import * as yup from 'yup';
+import { CMSTable, DatePicker } from 'components';
 
-// import './index.scss';
+import './index.scss';
+
+const schema = yup
+  .object({
+    title: yup.string().required(),
+    category: yup.mixed().required(),
+    thumbnail: yup.mixed().required(),
+  })
+  .required();
 
 const LogActivity = () => {
+  const {
+    control,
+    formState: { errors },
+    handleSubmit,
+  } = useForm({
+    resolver: yupResolver(schema),
+  });
   const history = useHistory();
   const LIST_TABLE = [
     {
@@ -78,18 +97,54 @@ const LogActivity = () => {
     },
   ];
   return (
-    <div className="container">
-      <CMSTable
-        customWidth={[20, 20, 20, 20, 20]}
-        header={['ID Pengguna', 'Alamat IP', 'Waktu', 'Aktivitas', 'Status']}
-        data={LIST_TABLE.map((item) => {
-          let value = {
-            data: [item.id_user, item.ip, item.time, item.activity, item.status],
-            action: '/cms/api-detail/' + item.id,
-          };
-          return value;
-        })}
-      />
+    <div className="log-activity">
+      <div className="container">
+        <div className="header-log">
+          <div className="wrapper-left">
+            <h1> Log Aktivitas </h1>
+            <Button className="" variant="info" style={{ width: '112px' }}>
+              Download
+            </Button>
+            <Button
+              className="ml-10 bg-white sdp-text-grey-dark border-gray-stroke secondary"
+              variant="secondary"
+              style={{ width: '112px' }}>
+              Backup
+            </Button>
+          </div>
+          <div className="wrapper-right">
+            <div className="date">
+              <p>Awal</p>
+              <DatePicker
+                name="publishedDate"
+                control={control}
+                rules={{ required: false }}
+                error={errors.publishedDate?.message}
+              />
+            </div>
+            <div className="date">
+              <p>Akhir</p>
+              <DatePicker
+                name="publishedDate"
+                control={control}
+                rules={{ required: false }}
+                error={errors.publishedDate?.message}
+              />
+            </div>
+          </div>
+        </div>
+        <CMSTable
+          customWidth={[20, 20, 20, 20, 20]}
+          header={['ID Pengguna', 'Alamat IP', 'Waktu', 'Aktivitas', 'Status']}
+          data={LIST_TABLE.map((item) => {
+            let value = {
+              data: [item.id_user, item.ip, item.time, item.activity, item.status],
+              action: '/cms/api-detail/' + item.id,
+            };
+            return value;
+          })}
+        />
+      </div>
     </div>
   );
 };

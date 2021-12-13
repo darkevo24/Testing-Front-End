7faf2d6aa6c35df97fb6fa9008e65969e3754d7b
anurdin <adamnurdin01@gmail.com>
2021-12-13 21:00:53 +0700

    fix: 🐛 Remove console from reducer

--- src/containers/CMS/PermintaanDataForm/reducer.js
+++ src/containers/CMS/PermintaanDataForm/reducer.js
@@ -14,7 +14,6 @@ export const PERMINTAAN_DATA_DETAIL = 'PERMINTAAN_DATA_DETAIL';
 export const getPermintaanDataDetail = createAsyncThunk('/permintaan-data/detail', async (params) => {
   let data = cloneDeep(params);
   const response = await get(apiUrls.detailPermintaanData + '/' + data);
-  console.log('response', response);
   return response?.data.content;
 });
 
@@ -46,7 +45,6 @@ const permintaanDataDetailSlice = createSlice({
     });
     builder.addCase(getPermintaanDataDetail.fulfilled, (state, action) => {
       state.loading = false;
-      console.log('action', action.payload);
       state.result = action.payload;
     });
     builder.addCase(getPermintaanDataDetail.rejected, (state, action) => {
@@ -59,7 +57,6 @@ const permintaanDataDetailSlice = createSlice({
     builder.addCase(postPermintaanDataTolak.fulfilled, (state, action) => {
       state.loading = false;
       state.result = action.payload;
-      console.log(action);
     });
     builder.addCase(postPermintaanDataTolak.rejected, (state, action) => {
       state.loading = false;

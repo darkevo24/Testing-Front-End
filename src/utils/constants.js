19582d268036b1552321362c53b18f0e95228e9c
Brijesh Bhakta <brijeshbhakta30@gmail.com>
2021-12-20 19:20:02 +0530

    feat: ✨ katalog variable details fetch for table

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -48,6 +48,8 @@ export const apiUrls = {
   katalogData: getV1Endpoint('katalog'),
   listPermintaanData: getApiEndpoint('sekretariat/permintaan-data'),
   detailPermintaanData: getApiEndpoint('sekretariat/permintaan-data'),
+  variable: getV1Endpoint('variable'),
+  katalogVariable: getV1Endpoint('variable/katalog'),
   daftarData: getV1Endpoint('katalog'),
   daftarDataList: getV1Endpoint('katalog/list'),
   daftarDataSummary: getV1Endpoint('katalog/summary'),
@@ -99,6 +101,8 @@ export const jadwalPermutakhiranOptions = JADWAL_PERMUTAKHIRAN.map(arrayToOption
 export const FORMATS = ['csv', 'xlsx', 'pdf', 'png/jpg/jpeg', 'docx', 'json', 'xml'];
 
 export const formatOptions = FORMATS.map(arrayToOptionsMapper());
+
+export const pengaturanAksesOptions = ['Terbuka', 'Terbatas', 'Tertutup'].map(arrayToOptionsMapper());
 
 export const TOPIC_LIST = [
   { title: 'Semua', items: [], icon: <SplitCircle /> },

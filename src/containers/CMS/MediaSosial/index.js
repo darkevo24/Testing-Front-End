e4706c14caaba1da2545f0cd76deb0119ecab21d
Eka DDM <eka.p@deltadatamandiri.com>
2021-12-24 18:32:53 +0700

    feat: ✨ sosmed create & edit

--- src/containers/CMS/MediaSosial/index.js
+++ src/containers/CMS/MediaSosial/index.js
@@ -1,27 +1,52 @@
 import React, { useEffect, useState } from 'react';
+import { useSelector, useDispatch } from 'react-redux';
+import { getListSosMed, getListSelector, deleteSosMed, editSosMed, createSosMed } from './reducer';
+
 import Button from 'react-bootstrap/Button';
 import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
-import bn from 'utils/bemNames';
-import cx from 'classnames';
-import { CMSModal, Modal } from 'components';
+import Form from 'react-bootstrap/Form';
+import { CMSModal, Modal, FileInput, Input, Loader } from 'components';
 import { icons, Trash, Edit } from 'components/Icons';
 
-import { useSelector, useDispatch } from 'react-redux';
-import { getListSosMed, getListSelector, deleteSosMed } from './reducer';
+import bn from 'utils/bemNames';
+import cx from 'classnames';
+import { useForm } from 'react-hook-form';
+import * as yup from 'yup';
+import { yupResolver } from '@hookform/resolvers/yup';
+import { apiUrls, post } from 'utils/request';
 
 const bem = bn('content-table');
 
 const CMSMediaSosial = () => {
+  const schema = yup
+    .object({
+      image: yup.mixed().required(),
+      tipe: yup.string().required(),
+    })
+    .required();
+
   const dispatch = useDispatch();
-  const { records } = useSelector(getListSelector);
+  const { loading, records } = useSelector(getListSelector);
   const [selected, setSelected] = useState(null);
   const [modalDelete, setModalDelete] = useState(false);
-  const [modalAdd, setModalAdd] = useState(false);
-  const [modalEdit, setModalEdit] = useState(false);
+  const [modalForm, setModalForm] = useState('');
+
+  const {
+    control,
+    formState: { errors },
+    handleSubmit,
+    setValue,
+  } = useForm({
+    resolver: yupResolver(schema),
+  });
+
+  const fetchData = () => {
+    dispatch(getListSosMed());
+  };
 
   useEffect(() => {
-    dispatch(getListSosMed());
+    fetchData();
   }, []);
 
   const deleteAction = (key) => {
@@ -29,13 +54,76 @@ const CMSMediaSosial = () => {
     setModalDelete(true);
   };
 
+  const editAction = (key) => {
+    setSelected(records[key]);
+    setValue('id', records[key].id);
+    setValue('tipe', records[key].tipe);
+    setValue('image', records[key].image);
+    setModalForm('edit');
+  };
+
+  const addAction = () => {
+    setValue('image', '');
+    setValue('tipe', '');
+    setModalForm('add');
+  };
+
+  const deleteData = () => {
+    dispatch(deleteSosMed({ id: selected.id })).then(() => {
+      fetchData();
+      setModalDelete(false);
+    });
+  };
+
+  const simpanData = (data) => {
+    setModalForm('');
+    // edit action
+    if (modalForm === 'edit') {
+      return dispatch(editSosMed({ payload: data })).then(() => fetchData());
+    }
+    // create action
+    delete data.id;
+    return dispatch(createSosMed({ payload: data })).then(() => fetchData());
+  };
+
+  const [foto, setFoto] = useState(null);
+  const [fotoLoading, setFotoLoading] = useState(false);
+  const handleFoto = (file) => {
+    // eslint-disable-next-line
+    let fileName = file.name.replace(/[&/\\#, +()$~%'":*?<>{}]/g, '');
+    let newFile = new File([file], fileName, { type: 'image/png' });
+    setFoto(newFile);
+  };
+  const uploadFoto = async () => {
+    setFotoLoading(true);
+    try {
+      let fotoFormData = new FormData();
+      fotoFormData.append('file', foto);
+      await post(apiUrls.uploadFoto, fotoFormData, { headers: { 'Content-Type': undefined } }).then((res) => {
+        setFotoLoading(false);
+        setSelected({
+          ...selected,
+          image: res.data.location,
+        });
+        setValue('image', res.data.location);
+      });
+    } catch (e) {
+      errors.foto.message = e?.error?.message;
+    }
+  };
+  useEffect(() => {
+    if (foto !== null) {
+      uploadFoto();
+    }
+  }, [foto]);
+
   return (
     <div className={bem.e('section')}>
       <div className={bem.e('header')}>
         <div className={cx(bem.e('title'), 'mb-3')}>Media Sosial</div>
         <Row className="justify-content-between">
           <Col xs={2}>
-            <Button onClick={() => setModalAdd(true)} variant="info" className="text-center">
+            <Button onClick={() => addAction()} variant="info" className="text-center">
               Tambah
             </Button>
           </Col>
@@ -43,8 +131,9 @@ const CMSMediaSosial = () => {
       </div>
       <div className={bem.e('body')}>
         {records.length === 0 ? (
-          <div className="text-center">
+          <div className="text-center mt-3">
             <icons.noPerminataanData />
+            <div className="text-black-50 mb-2 mt-2">No Data</div>
           </div>
         ) : null}
         {records.map((sosmed, index) => (
@@ -59,18 +148,55 @@ const CMSMediaSosial = () => {
               <div onClick={() => deleteAction(index)}>
                 <Trash />
               </div>
-              <Button onClick={() => setModalEdit(true)} variant="light" className="bg-white border-gray-stroke">
+              <Button onClick={() => editAction(index)} variant="light" className="bg-white border-gray-stroke">
                 <Edit />
               </Button>
             </Col>
           </Row>
         ))}
       </div>
+      {loading && <Loader fullscreen={true} />}
+      <Modal
+        visible={modalForm}
+        onClose={() => setModalForm('')}
+        title={modalForm === 'add' ? 'Tambah Sosial Media' : 'Edit Sosial Media'}>
+        <Form>
+          <Form.Group className="mb-3">
+            <Form.Label>Logo Sosmed</Form.Label>
+            <div>{modalForm === 'edit' ? <img className="wpx-50 mb-3" src={selected?.image} alt="logo" /> : null}</div>
+            <FileInput
+              name="image"
+              uploadInfo="Upload Image (format .png, .svg, 512KB)"
+              control={control}
+              handleOnChange={handleFoto}
+              error={errors.image?.message}
+            />
+          </Form.Group>
+          <Input group label="Nama Media Sosial" name="tipe" control={control} error={errors.tipe?.message} />
+          <div className="d-flex justify-content-end mb-12 mt-32">
+            <Button
+              onClick={() => setModalForm('')}
+              className="ml-24 bg-white sdp-text-grey-dark border-gray-stroke"
+              variant="light"
+              style={{ width: '112px' }}>
+              Batal
+            </Button>
+            <Button
+              onClick={handleSubmit(simpanData)}
+              disabled={fotoLoading}
+              className="ml-10"
+              variant="info"
+              style={{ width: '112px' }}>
+              Simpan
+            </Button>
+          </div>
+        </Form>
+      </Modal>
       {modalDelete ? (
         <CMSModal
           loader={false}
           onClose={() => setModalDelete(false)}
-          confirmButtonAction={() => dispatch(deleteSosMed({ id: selected.id }))}
+          confirmButtonAction={() => deleteData()}
           label={
             <span>
               Apakah anda yakin ingin <b className="sdp-text-blue">menghapus</b> {selected.tipe}?
--- src/containers/CMS/MediaSosial/reducer.js
+++ src/containers/CMS/MediaSosial/reducer.js
@@ -13,22 +13,27 @@ export const initialState = {
     totalRecords: 0,
     totalPages: 1,
   },
-  detaildataSet: {
-    loading: false,
-    error: '',
-    record: {},
-  },
 };
 
 export const SOSMED_SLICE = 'SOSMED_SLICE';
 
-export const getListSosMed = createAsyncThunk('cms/getListSosMed', async (params) => {
+export const getListSosMed = createAsyncThunk('cms/getListSosMed', async () => {
   const response = await get(apiUrls.sosialMedia);
   return response?.data?.content;
 });
 
 export const deleteSosMed = createAsyncThunk('cms/deleteSosMed', async (params) => {
   const response = await deleteRequest(`${apiUrls.sosialMedia}/${params.id}`);
+  return response?.data?.content;
+});
+
+export const createSosMed = createAsyncThunk('cms/createSosMed', async (params) => {
+  const response = await post(apiUrls.sosialMedia, params.payload);
+  return response?.data?.content;
+});
+
+export const editSosMed = createAsyncThunk('cms/editSosMed', async (params) => {
+  const response = await put(`${apiUrls.sosialMedia}/${params.payload.id}`, params.payload);
   return response?.data?.content;
 });
 
@@ -66,11 +71,32 @@ const sosmedSlice = createSlice({
       state.dataset.loading = false;
       state.dataset.error = action.error.message;
     });
+
+    builder.addCase(createSosMed.pending, (state) => {
+      state.dataset.loading = true;
+    });
+    builder.addCase(createSosMed.fulfilled, (state, action) => {
+      state.dataset.loading = false;
+    });
+    builder.addCase(createSosMed.rejected, (state, action) => {
+      state.dataset.loading = false;
+      state.dataset.error = action.error.message;
+    });
+
+    builder.addCase(editSosMed.pending, (state) => {
+      state.dataset.loading = true;
+    });
+    builder.addCase(editSosMed.fulfilled, (state, action) => {
+      state.dataset.loading = false;
+    });
+    builder.addCase(editSosMed.rejected, (state, action) => {
+      state.dataset.loading = false;
+      state.dataset.error = action.error.message;
+    });
   },
 });
 
 export const getListSelector = (state) => state.sosialMedia?.dataset;
-export const detailDataSelector = (state) => state.sosialMedia?.detaildataSet;
 
 export const { updateResult } = sosmedSlice.actions;
 export default sosmedSlice.reducer;

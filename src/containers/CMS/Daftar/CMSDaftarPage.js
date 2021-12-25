2eded84c2e9784392bafcd4778391766304eee17
Lakshit Shah <lakshitshah.nmims@gmail.com>
2021-12-25 23:11:02 +0530

    feat: ✨ create, edit, delete daftar data

--- src/containers/CMS/Daftar/CMSDaftarPage.js
+++ src/containers/CMS/Daftar/CMSDaftarPage.js
@@ -1,16 +1,19 @@
 import React, { useState } from 'react';
 import cx from 'classnames';
 import { useTranslation } from 'react-i18next';
-// import { useHistory } from 'react-router-dom';
-// import Button from 'react-bootstrap/Button';
+import { useHistory } from 'react-router-dom';
+import Button from 'react-bootstrap/Button';
 import { FilterSearchInput } from 'components/Table';
 import DaftarTable from 'containers/Daftar/DaftarTable';
+import { sekreteriatDaftarDataSelector } from 'containers/Daftar/reducer';
+import { useSelector } from 'react-redux';
 
 const CMSDaftarPage = (props) => {
   const [search, setSearch] = useState('');
   const { t } = useTranslation();
   const { bem } = props;
-  // const history = useHistory();
+  const history = useHistory();
+  const { bodyParams } = useSelector(sekreteriatDaftarDataSelector);
 
   return (
     <div className="daftar-page p-32 pb-100">
@@ -18,12 +21,12 @@ const CMSDaftarPage = (props) => {
         <div className="sdp-heading-big">{t('sandbox.daftar.title')}</div>
         <div className="d-flex justify-content-between mt-26">
           <div className="d-flex">
-            {/*<Button variant="info" className="text-nowrap" onClick={() => history.push('/cms/daftar/manage-dafter-data/')}>*/}
-            {/*  {t('common.addData')}*/}
-            {/*</Button>*/}
-            {/*<Button variant="secondary" className="ml-8 text-nowrap" onClick={() => {}}>*/}
-            {/*  {t('common.download')}*/}
-            {/*</Button>*/}
+            <Button variant="info" className="text-nowrap" onClick={() => history.push('/cms/daftar/manage-dafter-data/')}>
+              {t('common.addData')}
+            </Button>
+            <Button variant="secondary" className="ml-8 text-nowrap" onClick={() => props.onDownloadData(bodyParams)}>
+              {t('common.download')}
+            </Button>
           </div>
           <div>
             <FilterSearchInput

fb3be4e21b8d8bf13f11f5a74c51dbe15a013f94
pavangandhi9197 <pavangandhi9197@gmail.com>
2021-12-29 17:18:26 +0530

    fix: 🐛 resolved comments

--- src/components/Pagination.js
+++ src/components/Pagination.js
@@ -1,16 +1,8 @@
 import React, { useState, useEffect } from 'react';
-import styled from 'styled-components';
 import { LeftChevron, RightChevron } from 'components/Icons';
+import bn from 'utils/bemNames';
 
-const StyledButton = styled.button`
-  background: #ffffff;
-  border: 0px;
-  margin: 0px 7px;
-  vertical-align: middle;
-  justify-content: center;
-  align-items: center;
-`;
-
+const bem = bn('bimtek-pagination');
 const Pagination = ({ totalPages = 10, onChangePage }) => {
   const [pager, setPager] = useState(null);
 
@@ -36,25 +28,27 @@ const Pagination = ({ totalPages = 10, onChangePage }) => {
   };
 
   return (
-    <div className="float-end">
-      <StyledButton disabled={pager?.currentPage === 1} onClick={() => setPage((pager?.currentPage || 1) - 1)}>
+    <div className="bimtek-pagination float-end">
+      <button
+        className={bem.e('bimtek-pagination-buttons')}
+        disabled={pager?.currentPage === 1}
+        onClick={() => setPage((pager?.currentPage || 1) - 1)}>
         <LeftChevron />
-      </StyledButton>
+      </button>
       {(pager?.pages || []).map((page, index) => (
-        <StyledButton
+        <button
           key={index}
           onClick={() => setPage(page)}
-          style={{
-            color: (pager?.currentPage || 1) === page ? '#000000' : '#B9B9B9',
-          }}>
+          className={bem.e((pager?.currentPage || 1) === page ? 'bimtek-active-page' : 'bimtek-inactive-page')}>
           {page}
-        </StyledButton>
+        </button>
       ))}
-      <StyledButton
+      <button
+        className={bem.e('bimtek-pagination-buttons')}
         disabled={(pager?.currentPage || 1) === (pager?.totalPages || 1)}
         onClick={() => setPage((pager?.currentPage || 1) + 1)}>
         <RightChevron />
-      </StyledButton>
+      </button>
     </div>
   );
 };

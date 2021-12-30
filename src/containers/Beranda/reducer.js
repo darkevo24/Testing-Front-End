44baac723bc202e8b4e7333716beebee796139c4
Brijesh Bhakta <brijeshbhakta30@gmail.com>
2021-12-30 16:52:56 +0530

    fix: 🐛 dataset kategori filter with space not working

--- src/containers/Beranda/reducer.js
+++ src/containers/Beranda/reducer.js
@@ -60,6 +60,8 @@ export const getDataSet = createAsyncThunk('beranda/getDataset', async (params, 
     }
     delete data.kategori;
     data.q = data.q.filter(Boolean).join('&');
+  } else if (data.kategori?.length) {
+    data.kategori = data.kategori.map(({ id }) => ({ id: id.replace(/ /g, '*') }));
   }
   data = mapParamsToJsonString(data, ['facet.field']);
   data = mapParamsToOrString(data, facetFields);

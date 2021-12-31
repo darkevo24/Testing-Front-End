8217bdc0a6a5121a69032ea00e0a133098605577
Eka DDM <eka.p@deltadatamandiri.com>
2021-12-31 11:52:43 +0700

    feat: ✨ contact us sosmed

--- src/containers/CMS/ContactUs/form.js
+++ src/containers/CMS/ContactUs/form.js
@@ -1,12 +1,17 @@
+import React, { useState } from 'react';
 import { useForm } from 'react-hook-form';
 import { yupResolver } from '@hookform/resolvers/yup';
 import * as yup from 'yup';
 import Button from 'react-bootstrap/Button';
 import Form from 'react-bootstrap/Form';
+import InputGroup from 'react-bootstrap/InputGroup';
 import { Input } from 'components';
-import { NoPerminataanData } from 'components/Icons';
+import { NoPerminataanData, Trash } from 'components/Icons';
 import { ReactComponent as Plus } from 'assets/plus.svg';
+import { Kontak_list } from 'utils/constants';
 import { submitForm } from 'utils/helper';
+
+import CMSContactSosmed from './formSosmed';
 
 const schema = yup
   .object({
@@ -29,8 +34,32 @@ const CMSContactForm = ({ data, disabled, onSubmit = () => {} }) => {
     },
   });
 
+  const [modalOpen, setModalOpen] = useState(false);
+  const [listSosmed, setListSosmed] = useState(data.sosialMedia);
+  const addSosmed = (data) => {
+    setListSosmed([...listSosmed, data]);
+    setModalOpen(false);
+  };
+
+  const submitContact = (data) => {
+    data.sosialMedia = listSosmed;
+    onSubmit(data);
+  };
+
+  const removeSosmed = (index) => {
+    const data = listSosmed[index];
+    setListSosmed(listSosmed.filter((item) => item !== data));
+  };
+
+  const changeSosmed = (e, index) => {
+    const data = listSosmed[index];
+    let edit = [...listSosmed];
+    edit[index] = { ...data, url: e.target.value };
+    setListSosmed(edit);
+  };
+
   return (
-    <Form id={formContactId} onSubmit={handleSubmit(onSubmit)}>
+    <Form id={formContactId} onSubmit={handleSubmit(submitContact)}>
       <Input
         group
         label="Alamat Line 1"
@@ -68,7 +97,7 @@ const CMSContactForm = ({ data, disabled, onSubmit = () => {} }) => {
         <div className="d-flex justify-content-between mb-3">
           <div className="fs-16 fw-bold">Tautan Sosial Media</div>
           {!disabled && (
-            <div className="sdp-text-red fw-600 cursor-pointer">
+            <div onClick={() => setModalOpen(true)} className="sdp-text-red fw-600 cursor-pointer">
               <Plus width="16" /> Tambah
             </div>
           )}
@@ -80,11 +109,27 @@ const CMSContactForm = ({ data, disabled, onSubmit = () => {} }) => {
               <div className="text-black-50 mb-2 mt-2">No Data</div>
             </div>
           ) : (
-            data.sosialMedia.map((sosmed, index) => <div key={index}>{sosmed.tipe}</div>)
+            listSosmed.map((sosmed, index) => (
+              <InputGroup key={index} className="mb-3">
+                <InputGroup.Text className="bg-white">
+                  <img src={sosmed.image} alt="logo" />
+                </InputGroup.Text>
+                <Form.Control disabled={disabled} defaultValue={sosmed.url} onChange={(e) => changeSosmed(e, index)} />
+                {!disabled ? (
+                  <InputGroup.Text className="bg-white">
+                    <Form.Check className="mr-16" />
+                    <span className="cursor-pointer" onClick={() => removeSosmed(index)}>
+                      <Trash />
+                    </span>
+                  </InputGroup.Text>
+                ) : null}
+              </InputGroup>
+            ))
           )}
         </div>
       </div>
       <Button className="invisible" type="submit" />
+      {modalOpen ? <CMSContactSosmed handleClose={() => setModalOpen(false)} onSubmit={addSosmed} /> : null}
     </Form>
   );
 };

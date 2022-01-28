9edc77771d4d6d1d8f1fe1ef509ae469e14fc84d
yehualashet <yehualashetab@gmail.com>
2022-01-28 03:58:22 +0300

    feat: logging user out for not accepting term and condition   * loggin user out if s/he fails to accept the term and condition and pass recaptcha validation

--- src/layouts/index.js
+++ src/layouts/index.js
@@ -54,7 +54,10 @@ export const PrivateRoute = ({ component: Component, path, permissions, ...rest 
   const token = useSelector(tokenSelector) || getCookieByName(cookieKeys.token);
   const { keycloak } = useKeycloak();
   const isTermAndConditionAccepted = useSelector(termAndConditionSelector);
-  if (!isTermAndConditionAccepted) keycloak.logout();
+  if (!isTermAndConditionAccepted) {
+    keycloak.logout();
+    return <Redirect to="/home" />;
+  }
   if (isArrayWithLength(permissions)) {
     let allowedRoutes = [];
     if (token) allowedRoutes = getAllowedRoutes(permissions);

485b73e440f8984c9192a72d47c3fee6545638fe
Yehualashet <yehualashetab@gmail.com>
2022-02-04 09:03:15 +0300

    feat: ✨ update not Found Page

--- src/containers/NotFound/index.js
+++ src/containers/NotFound/index.js
@@ -1,7 +1,8 @@
 import Button from 'react-bootstrap/Button';
 import { useHistory } from 'react-router-dom';
 
-import image404 from 'assets/404.png';
+import image404 from 'assets/no-akses.svg';
+
 import bn from 'utils/bemNames';
 
 const bem = bn('404');
@@ -12,7 +13,7 @@ export const NotFound = () => {
   return (
     <div className={bem.b()}>
       <img src={image404} className={bem.e('img-not-found')} alt="not-found" />
-      <div className="sdp-heading my-24">Telah terjadi kesalahan, halaman yang anda tuju tidak tersedia untuk saat ini</div>
+      <div className="sdp-heading my-24">Maaf, akun anda tidak memiliki akses untuk menggunakan fitur/layanan ini</div>
       <Button onClick={goTo('/home')}>Kembali ke Home</Button>
     </div>
   );

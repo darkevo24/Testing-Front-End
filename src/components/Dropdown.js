9118fdc37b771a59b9814f3d7fbda1d74b22a5fe
yehualashet <yehualashetab@gmail.com>
2022-04-07 11:48:19 +0300

    feat: ✨ handle unselect of an element in dropdown

--- src/components/Dropdown.js
+++ src/components/Dropdown.js
@@ -6,6 +6,7 @@ import isArray from 'lodash/isArray';
 import isString from 'lodash/isString';
 import find from 'lodash/find';
 import map from 'lodash/map';
+import findIndex from 'lodash/findIndex';
 import uniqBy from 'lodash/uniqBy';
 import { Check } from 'components/Icons';
 
@@ -36,7 +37,19 @@ export const Dropdown = ({
             if (multi) {
               // TODO: handle unselect of an element
               const currentFieldValue = field.value && isArray(field.value) ? field.value : [];
-              currentFieldValue.push(option);
+              // check if the option is already selected
+              const isSelected = findIndex(currentFieldValue, (item) => item.value === option.value);
+              if (isSelected !== -1) {
+                // remove the option
+                currentFieldValue.splice(isSelected, 1);
+                if (!currentFieldValue.length) {
+                  field.onChange(null);
+                  return;
+                }
+              } else {
+                // add the option
+                currentFieldValue.push(option);
+              }
               field.onChange(uniqBy(currentFieldValue, 'value'));
               return;
             }

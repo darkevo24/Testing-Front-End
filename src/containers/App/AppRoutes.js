c83dae12f5e4e3b77fc2b3a6b333bcf3bc920ba3
yehualashet <yehualashetab@gmail.com>
2022-04-18 11:10:17 +0300

    feat: ✨  allow user EKSEKUTIF role to chnage thier password

--- src/containers/App/AppRoutes.js
+++ src/containers/App/AppRoutes.js
@@ -127,6 +127,7 @@ function AppRoutes(props) {
               component={ChangePasswordUser}
               permissions={[
                 Roles.MEMBER,
+                Roles.EKSEKUTIF,
                 Roles.CONTENT_CREATOR,
                 Roles.CONTENT_EDITOR,
                 Roles.SEKRETARIANT,

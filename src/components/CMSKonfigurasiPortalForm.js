642becf75c8e3128191051d4006ea5d99608ae45
dadang <dadang@desktopip.com>
2022-05-11 22:46:03 +0700

    #3 add form konfigurasi portal

---
+++ src/components/CMSKonfigurasiPortalForm.js
@@ -0,0 +1,294 @@
+import React, { useEffect, useState } from 'react';
+import Form from 'react-bootstrap/Form';
+import Col from 'react-bootstrap/Col';
+import Row from 'react-bootstrap/Row';
+import Button from 'react-bootstrap/Button';
+import { useForm } from 'react-hook-form';
+import { yupResolver } from '@hookform/resolvers/yup';
+import * as yup from 'yup';
+import { Input, FileInput, TextEditor, RequiredFilledLabel } from 'components';
+import MultiSelectDropDown from 'components/DropDown/MultiSelectDropDown';
+import SingleSelectDropdown from 'components/DropDown/SingleSelectDropDown';
+import { submitForm } from 'utils/helper';
+import { useDispatch, useSelector } from 'react-redux';
+import { apiUrls, post } from 'utils/request';
+import { ComponentAccessibility } from 'components/ComponentAccess';
+import { USER_ROLES } from 'utils/constants';
+import { getListKategori, getListTagline, setNewTagline, kategoriSelector, taglineSelector } from 'containers/App/reducer';
+
+export const beritaFormId = 'berita-form-id';
+export const submitBeritaForm = submitForm(beritaFormId);
+
+export const submitNewKategori = async (kategori) => {
+  try {
+    const param = {
+      kode: 'BERITA',
+      keterangan: kategori,
+      idParent: null,
+      status: true,
+    };
+    const response = await post(apiUrls.setting, param);
+    return response;
+  } catch (e) {
+    // eslint-disable-next-line
+    console.log(e);
+  }
+};
+
+export const getDate = (date) => {
+  if (!date) {
+    return '';
+  }
+  // handle format yyyy-mm-ddTHH:mm:ss
+  if (date.indexOf('T') >= 0) {
+    return date.split('T')[0];
+  }
+
+  // handle format yyyy-mm-dd HH:mm:ss
+  return date.split(' ')[0];
+};
+
+export const getTime = (date) => {
+  if (!date) {
+    return '';
+  }
+  // handle format yyyy-mm-ddTHH:mm:ss
+  if (date.indexOf('T') >= 0) {
+    return date.split('T')[1].split(' ')[0].split('.')[0];
+  }
+
+  // handle format yyyy-mm-dd HH:mm:ss
+  return date.split(' ')[1];
+};
+
+const schema = yup
+  .object({
+    judul: yup.string().required(),
+    kategori: yup.mixed().required(),
+    mainImage: yup.mixed().required(),
+    content: yup.mixed().required(),
+  })
+  .required();
+
+const CMSKonfigurasiPortalForm = ({ data, style, onSubmit, disabled = false }) => {
+  const dispatch = useDispatch();
+  const [listKategori, setListKategori] = useState([]);
+  const { records: kategoriRecords } = useSelector(kategoriSelector);
+  const { records: taglineRecords } = useSelector(taglineSelector);
+
+  useEffect(() => {
+    setListKategori(kategoriRecords);
+  }, [kategoriRecords]);
+
+  const createKategori = (data) => {
+    setValue('kategori', { id: 'new', value: 'new', label: data });
+    setListKategori([
+      ...listKategori,
+      {
+        id: 'new',
+        keterangan: data,
+      },
+    ]);
+  };
+
+  const createTagline = (data) => {
+    dispatch(setNewTagline({ keterangan: data }))
+      .then((res) => {
+        let currentTag = getValues('taglineId') || [];
+        currentTag.push({ value: res.payload.id, label: res.payload.keterangan });
+        setValue('taglineId', currentTag);
+      })
+      .then(() => dispatch(getListTagline()));
+  };
+
+  const {
+    control,
+    formState: { errors },
+    handleSubmit,
+    setValue,
+    getValues,
+    setError,
+  } = useForm({
+    resolver: yupResolver(schema),
+    defaultValues: {
+      ...data,
+      kategori: !data.kategori
+        ? null
+        : data.kategori.value
+        ? data.kategori
+        : {
+            value: data.kategori,
+            label: kategoriRecords.find((kategori) => kategori.id === data.kategori)?.keterangan,
+          },
+      taglineId: data.tagLineList?.map((tagline) => ({ label: tagline.keterangan, value: tagline.id })),
+    },
+  });
+
+  useEffect(() => {
+    dispatch(getListKategori('BERITA'));
+    dispatch(getListTagline());
+  }, []);
+
+  const [foto, setFoto] = useState(null);
+  const handleFoto = (file) => {
+    // eslint-disable-next-line
+    let fileName = file.name.replace(/[&\/\\#, +()$~%'":*?<>{}]/g, '');
+    let blob = file;
+    let newFile = new File([blob], fileName, { type: 'image/png' });
+    setFoto(newFile);
+  };
+  const uploadFoto = async () => {
+    try {
+      let fotoFormData = new FormData();
+      fotoFormData.append('file', foto);
+      await post(apiUrls.uploadFoto, fotoFormData, { headers: { 'Content-Type': undefined } }).then((res) => {
+        setValue('mainImage', res.data.location);
+      });
+    } catch (e) {
+      setError('mainImage', {
+        type: 'manual',
+        message: e.error?.message,
+      });
+    }
+  };
+
+  useEffect(() => {
+    if (foto !== null) {
+      uploadFoto();
+    }
+  }, [foto]);
+
+  return (
+    <Form id={beritaFormId} className="sdp-form" onSubmit={handleSubmit(onSubmit)} style={style}>
+      <h5>Informasi</h5>
+      {disabled ? (
+        <img className="wpx-200 mb-3" src={data?.mainImage} alt="thumbnail" />
+      ) : (
+        <FileInput
+          group
+          label="Thumbnail"
+          name="mainImage"
+          control={control}
+          error={errors.mainImage?.message}
+          uploadInfo="Upload Image (format .png, .jpeg, .jpg max. 512KB)"
+          handleOnChange={handleFoto}
+        />
+      )}
+      <Input group label="Judul" name="judul" control={control} disabled={disabled} error={errors.judul?.message} />
+      <Input
+        groupClass="mb-16"
+        groupProps={{
+          md: 12,
+          as: Col,
+        }}
+        labelClass="sdp-form-label fw-normal"
+        group
+        label="Nama Organisasi"
+        error={errors?.linkAkses?.message}
+        name="linkAkses"
+        rightIcon="edit"
+        control={control}
+      />
+      <Input
+        groupClass="mb-16"
+        groupProps={{
+          md: 12,
+          as: Col,
+        }}
+        labelClass="sdp-form-label fw-normal"
+        group
+        label="Alamat Organisasi"
+        error={errors?.linkAkses?.message}
+        name="linkAkses"
+        rightIcon="edit"
+        control={control}
+      />
+      <Input
+        groupClass="mb-16"
+        groupProps={{
+          md: 12,
+          as: Col,
+        }}
+        labelClass="sdp-form-label fw-normal"
+        group
+        label="No Telpepon"
+        error={errors?.linkAkses?.message}
+        name="linkAkses"
+        rightIcon="edit"
+        control={control}
+      />
+      <Input
+        groupClass="mb-16"
+        groupProps={{
+          md: 12,
+          as: Col,
+        }}
+        labelClass="sdp-form-label fw-normal"
+        group
+        label="No Fax"
+        error={errors?.linkAkses?.message}
+        name="linkAkses"
+        rightIcon="edit"
+        control={control}
+      />
+      <Input
+        groupClass="mb-16"
+        groupProps={{
+          md: 12,
+          as: Col,
+        }}
+        labelClass="sdp-form-label fw-normal"
+        group
+        error={errors?.linkAkses?.message}
+        name="linkAkses"
+        leftIcon="facebookSvg"
+        rightIcon="edit"
+        control={control}
+      />
+      <Input
+        groupClass="mb-16"
+        groupProps={{
+          md: 12,
+          as: Col,
+        }}
+        labelClass="sdp-form-label fw-normal"
+        group
+        error={errors?.linkAkses?.message}
+        name="linkAkses"
+        leftIcon="twitterSvg"
+        rightIcon="edit"
+        control={control}
+      />
+      <Input
+        groupClass="mb-16"
+        groupProps={{
+          md: 12,
+          as: Col,
+        }}
+        labelClass="sdp-form-label fw-normal"
+        group
+        error={errors?.linkAkses?.message}
+        name="linkAkses"
+        leftIcon="instgramSvg"
+        rightIcon="edit"
+        control={control}
+      />
+      <Input
+        groupClass="mb-16"
+        groupProps={{
+          md: 12,
+          as: Col,
+        }}
+        labelClass="sdp-form-label fw-normal"
+        group
+        error={errors?.linkAkses?.message}
+        name="linkAkses"
+        leftIcon="youtubeSvg"
+        rightIcon="edit"
+        control={control}
+      />
+    </Form>
+  );
+};
+
+export default CMSKonfigurasiPortalForm;
---
+++ src/containers/CMS/KonfigurasiPortal/index.js
@@ -0,0 +1,190 @@
+import React, { useState } from 'react';
+import Button from 'react-bootstrap/Button';
+import { useHistory } from 'react-router-dom';
+
+import CMSForm, { submitBeritaForm, submitNewKategori, getDate } from 'components/CMSForm';
+import { CMSModal, Loader } from 'components';
+import Notification from 'components/Notification';
+import bn from 'utils/bemNames';
+import cx from 'classnames';
+import { setNewBerita, setPreviewBerita, detailDataSelector, setEditBerita, setStatusBerita } from './reducer';
+import { useDispatch, useSelector } from 'react-redux';
+import CMSKonfigurasiPortalForm from 'components/CMSKonfigurasiPortalForm';
+
+const bem = bn('content-create');
+
+const CMSKonfigurasiPortal = () => {
+  const history = useHistory();
+  const dispatch = useDispatch();
+  const [action, setAction] = useState('');
+  const [modalConfirm, setModalConfirm] = useState(false);
+  const [modalLabel, setModalLabel] = useState('');
+  const [dataBerita, setDataBerita] = useState({});
+  const { loading, record } = useSelector(detailDataSelector);
+
+  const onSubmit = (data) => {
+    // preview berita
+    if (action === 'preview') {
+      return dispatch(setPreviewBerita(data)).then(() => history.push('/berita/preview'));
+    }
+
+    data.mainImageTitle = '';
+    const publishDate = getDate(data.publishDate);
+    const publishTime = data.publishTime ? data.publishTime : '';
+    data.publishDate = !publishDate
+      ? ''
+      : !publishTime
+      ? publishDate + ' 00:00:00'
+      : publishDate + ' ' + publishTime + ':00';
+    data.taglineId = data.taglineId?.map((tag) => tag.value) || [];
+    data.issn = data.issn ? data.issn : '';
+
+    if (data.kategori.value !== 'new') {
+      data.kategori = data.kategori.value;
+    }
+
+    data.status = 0;
+    if (action === 'kirim') {
+      data.status = 2;
+    }
+    // set data
+    setDataBerita(data);
+
+    // open modal
+    let label = '';
+    switch (action) {
+      case 'simpan':
+        label = <span>Simpan Berita?</span>;
+        break;
+      case 'kirim':
+        label = <span>Kirim Berita?</span>;
+        break;
+      default:
+        return;
+    }
+    setModalLabel(label);
+    setModalConfirm(true);
+  };
+
+  const submitBerita = () => {
+    setModalConfirm(false);
+
+    // create new kategori
+    if (dataBerita.kategori?.id && dataBerita.kategori.id === 'new') {
+      submitNewKategori(dataBerita.kategori.label).then((res) => sendData({ ...dataBerita, kategori: res.data.content.id }));
+      return;
+    }
+    sendData(dataBerita);
+  };
+
+  const sendData = (data) => {
+    if (data.id) {
+      // edit berita
+      if (data.status === 2) {
+        data.status = 0;
+        return dispatch(setEditBerita({ payload: data, id: data.id }))
+          .then(() =>
+            dispatch(
+              setStatusBerita({
+                payload: { id: [data.id], status: 2, note: '' },
+              }),
+            ),
+          )
+          .then((res) => notifyResponse(res));
+      }
+      return dispatch(setEditBerita({ payload: data, id: data.id })).then((res) => notifyResponse(res));
+    }
+    // kirim
+    if (dataBerita.status === 2) {
+      return dispatch(setNewBerita({ payload: dataBerita }))
+        .then((res) =>
+          dispatch(
+            setStatusBerita({
+              payload: { id: [res.payload.content.id], status: 2, note: '' },
+            }),
+          ),
+        )
+        .then((res) => notifyResponse(res));
+    }
+    // simpan
+    dispatch(setNewBerita({ payload: dataBerita })).then((res) => notifyResponse(res));
+  };
+
+  const notifyResponse = (res) => {
+    res?.payload
+      ? Notification.show({
+          type: 'secondary',
+          message: (
+            <div>
+              Berita <span className="fw-bold">{res.meta.arg?.payload?.judul}</span> Berhasil Disimpan
+            </div>
+          ),
+          icon: 'check',
+          onClose: history.goBack(),
+        })
+      : Notification.show({
+          message: (
+            <div>
+              Error <span className="fw-bold">{res.error?.message}</span> Data Tidak Diubah
+            </div>
+          ),
+          icon: 'cross',
+        });
+  };
+
+  const actionClick = (status) => {
+    Promise.resolve()
+      .then(() => setAction(status))
+      .then(() => submitBeritaForm());
+  };
+
+  return (
+    <div className={bem.e('section')}>
+      <div className={cx(bem.e('header'), 'd-flex justify-content-between')}>
+        <div className={bem.e('title')}>
+          Aset
+          {/* <Button
+            onClick={() => dispatch(setPreviewBerita({})).then(() => history.goBack())}
+            className="ml-24"
+            variant="secondary"
+            style={{ width: '112px' }}>
+            Batal
+          </Button>
+          <Button
+            onClick={() => actionClick('simpan')}
+            className="ml-10 bg-white sdp-text-grey-dark border-gray-stroke"
+            variant="light"
+            style={{ width: '112px' }}>
+            Simpan
+          </Button>
+          <Button onClick={() => actionClick('kirim')} className="ml-10" variant="info" style={{ width: '112px' }}>
+            Kirim
+          </Button> */}
+        </div>
+        {/* <div>
+          <Button
+            onClick={() => actionClick('preview')}
+            className="ml-24 bg-white sdp-text-grey-dark border-gray-stroke"
+            variant="light"
+            style={{ width: '112px' }}>
+            Preview
+          </Button>
+        </div> */}
+      </div>
+      <div className={bem.e('body')}>
+        <CMSKonfigurasiPortalForm data={record} onSubmit={onSubmit} />
+      </div>
+      {modalConfirm ? (
+        <CMSModal
+          loader={false}
+          onClose={() => setModalConfirm(false)}
+          confirmButtonAction={submitBerita}
+          label={modalLabel}
+        />
+      ) : null}
+      {loading && <Loader fullscreen={true} />}
+    </div>
+  );
+};
+
+export default CMSKonfigurasiPortal;

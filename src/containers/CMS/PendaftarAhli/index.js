6b6d4514fc01c0859b17d4fc80eb39a8532c71a2
dadang <dadang@desktopip.com>
2022-06-27 04:56:45 +0700

    #18 fix pendaftar ahli cms page

--- src/containers/CMS/PendaftarAhli/index.js
+++ src/containers/CMS/PendaftarAhli/index.js
@@ -1,18 +1,13 @@
+import { useSelector } from 'react-redux';
+import { userSelector } from 'containers/Login/reducer';
+
 const PendaftarAhli = () => {
+  const user = useSelector(userSelector);
+  const src = 'https://bpm.satudata.go.id/#/PendaftaranAhli?userEmail=' + user?.email;
   return (
     <div className="py-40 px-0">
-      <div className="px-30 border-bottom">
-        <h1>Pendaftar Ahli</h1>
-      </div>
       <div className="pt-0">
-        <iframe
-          frameBorder="0"
-          width="100%"
-          height="700px"
-          frameBorder="0"
-          seamless
-          title="Pendaftar Ahli"
-          src="https://bpm.satudata.go.id/#/PenambahanAtributCMS"></iframe>
+        <iframe frameBorder="0" width="100%" height="700px" seamless title="Pendaftar Ahli" src={src}></iframe>
       </div>
     </div>
   );

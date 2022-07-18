ae58b3ff820ca74eb7607432c32498da86b6f36b
Dadang <hey.dadangkurniawan@gmail.comm>
2022-07-18 16:31:09 +0700

    #35 fix wavemaker button daftar menjadi ahli

--- src/containers/DaftarMenjadiAhli/index.js
+++ src/containers/DaftarMenjadiAhli/index.js
@@ -6,15 +6,15 @@ const DaftarMenjadiAhli = (props) => {
   const src = bpmUrl.concat('FormRequest');
   return (
     <div className="dashboard">
-      <div className="p-32">
+      <div className="pt-32">
         <Row>
           <Col>
             <iframe
               frameBorder="0"
               seamless
               title="Daftar menjadi ahli"
-              height="100%"
-              scrolling="no"
+              height="2200px"
+              scrolling="yes"
               width="100%"
               src={src}></iframe>
           </Col>

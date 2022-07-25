6ffcd17680959cd63912e8e73109e6e591c8ba7e
dadang <dadang@desktopip.com>
2022-07-25 15:53:56 +0700

    #42 add email on forum sdi

--- src/containers/SDIWiki/index.js
+++ src/containers/SDIWiki/index.js
@@ -1,7 +1,10 @@
+import { userSelector } from 'containers/Login/reducer';
+import { useSelector } from 'react-redux';
 import { bpmUrl } from 'utils/constants';
 
-const src = bpmUrl.concat('Wiki');
 const SDIWiki = () => {
+  const user = useSelector(userSelector);
+  const src = bpmUrl.concat('Wiki?userEmail=' + user?.email);
   return (
     <div>
       <iframe frameBorder="0" width="100%" height="700px" seamless title="Wiki" src={src}></iframe>

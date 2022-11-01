db02ffa2d06fc13e8b53235eae4839cf5c3ad1fb
Maliha Rahmani <amaliharahmani@gmail.com>
2022-11-01 23:57:48 +0700

    feat: ✨ accordion berita lain

--- src/containers/Berita/BeritaLainnya.js
+++ src/containers/Berita/BeritaLainnya.js
@@ -4,25 +4,80 @@ import { useHistory } from 'react-router-dom';
 import { useSelector, useDispatch } from 'react-redux';
 import { SectionTitle } from '.';
 import { getMonthlyNews, monthlyNewsSelector } from './reducer';
-import { SectionList } from 'components';
-import RBDropdown from 'react-bootstrap/Dropdown';
-import RBDropdownButton from 'react-bootstrap/DropdownButton';
 import Accordion from 'react-bootstrap/Accordion';
 
 const Wrapper = styled.div`
   margin-bottom: 40px;
 `;
 
-const TopikItem = styled.div`
-  border-bottom: 1px solid #e1e2ea;
-  padding: 16px;
+const AccordionItem = styled(Accordion.Item)`
+  width: 100%;
+  border: 0;
+  display: flex;
+`;
 
-  & span {
-    margin-right: 8px;
+const AccordionHeader = styled(Accordion.Header)`
+  margin-top: 4px;
+  border: 1px solid #e2e8f0;
+  cursor: pointer;
+  border-radius: 4px;
+  padding-left: 0;
+
+  & .accordion-button {
+    padding: 0.5rem 1.25rem;
+    height: 30px;
+    font-size: 12px;
+    font-weight: 600;
+    &:not(.collapsed) {
+      background-color: #ed1c24;
+      border: 0;
+      color: #ffffff;
+    }
+    &:after {
+      display: none;
+    }
+    &:focus {
+      border-color: transparent;
+      box-shadow: none;
+    }
   }
+`;
 
-  &:nth-child(2) {
-    border-top: 1px solid #e1e2ea;
+const Line = styled.div`
+  border-left: 1px solid #e2e8f0;
+  height: 100%;
+`;
+const Dot = styled.div`
+  height: 11px;
+  width: 10px;
+  background-color: #e2e8f0;
+  border-radius: 50%;
+  display: inline-block;
+
+  &.active {
+    background-color: transparent;
+    border: 1px solid #ed1c24;
+  }
+`;
+
+const Click = styled.div`
+  cursor: pointer;
+  width: 100%;
+  height: 100%;
+  flex: 1;
+`;
+
+const AccordionBody = styled(Accordion.Body)`
+  border-right-style: none;
+  cursor: pointer;
+  font-size: 12px;
+  padding: 0.5rem 1rem;
+  &:hover {
+    background-color: #f5f5f5;
+    border-radius: 4px;
+  }
+  .accordion-group {
+    border: none;
   }
 `;
 
@@ -30,38 +85,71 @@ const BeritaLainnya = () => {
   const dispatch = useDispatch();
   const { records, status } = useSelector(monthlyNewsSelector);
   const history = useHistory();
-
-  const [selectedOption, setSelectedOption] = useState(null);
+  const [datas, setDatas] = useState([]);
 
   useEffect(() => {
     if (status === 'idel') {
       dispatch(getMonthlyNews('/perbulan'));
     }
+
+    // create new object list from records to datas;
+    constructDatas(records);
   }, [dispatch, status]);
 
-  // const handleDetail = (event, date) => {
-  //   event.preventDefault();
-  //   history.push(`/berita/perbulan/${date}`);
-  // };
+  const constructDatas = (records) => {
+    const newDatas = Object.entries(records).map(([year, months]) => {
+      return {
+        year,
+        months,
+        expanded: false,
+      };
+    });
+    setDatas(newDatas);
+  };
 
-  // const onSortChange = (option) => () => {
-  //   setSelectedOption(option);
-  //   console.log(option);
-  // };
+  const handleDetail = (event) => {
+    const date = `${event.month}-${event.year}`;
+    history.push(`/berita/perbulan/${date}`);
+  };
+
+  const handleExpanded = (index) => {
+    const newDatas = [...datas];
+    newDatas[index].expanded = !newDatas[index].expanded;
+    setDatas(newDatas);
+  };
 
   return (
     <Wrapper>
-      <SectionTitle title="Berita Lainnya" />
-      <Accordion defaultActiveKey={['2021']} alwaysOpen>
-        {Object.entries(records).map(([key, value]) => (
-          <Accordion.Item eventKey={key}>
-            <Accordion.Header>{key}</Accordion.Header>
-            {value.map((item, index) => (
-              <Accordion.Body>
-                {item.bulan} ({item.jumlah})
-              </Accordion.Body>
-            ))}
-          </Accordion.Item>
+      <SectionTitle>Berita Lainnya</SectionTitle>
+      <Accordion alwaysOpen>
+        {datas.map((data, index) => (
+          <AccordionItem eventKey={data.year} className="row">
+            <div className="col-2 d-flex flex-column align-items-center">
+              <Dot className={`${data.expanded ? 'active' : ''}`} id="dot" />
+              <Line />
+            </div>
+            <div className="col-10 pl-0">
+              <AccordionHeader>
+                <Click onClick={() => handleExpanded(index)}>
+                  {data.year}
+                  <span className="fw-normal ml-5">
+                    (
+                    {data.months.reduce(function (tot, arr) {
+                      return tot + arr.jumlah;
+                    }, 0)}
+                    )
+                  </span>
+                </Click>
+              </AccordionHeader>
+              {data.months.map((item) => (
+                <AccordionBody key={item.bulan}>
+                  <Click onClick={() => handleDetail(item)}>
+                    {item.bulan} <span style={{ color: '#858a8f' }}>({item.jumlah})</span>
+                  </Click>
+                </AccordionBody>
+              ))}
+            </div>
+          </AccordionItem>
         ))}
       </Accordion>
     </Wrapper>

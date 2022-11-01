78bccb3a0e84d296dbcc27960a1105211b0e8f50
Imran Pulungan <imranpulungan7@gmail.com>
2022-11-01 22:51:04 +0000

    Contact us

--- src/containers/TentangSDI/form.js
+++ src/containers/TentangSDI/form.js
@@ -1,6 +1,10 @@
 import React, { useEffect, useRef, useState } from 'react';
 import Form from 'react-bootstrap/Form';
 import Button from 'react-bootstrap/Button';
+import Container from 'react-bootstrap/Container';
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
+
 import { postTentang } from './reducer';
 import { useForm } from 'react-hook-form';
 import { yupResolver } from '@hookform/resolvers/yup';
@@ -8,20 +12,22 @@ import { useDispatch } from 'react-redux';
 import * as yup from 'yup';
 import Notification from '../../components/Notification';
 import axios from 'axios';
-import Attachment from './attachment';
-import { apiUrls, fileExtention } from 'utils/constants';
+import { fileExtention } from 'utils/constants';
 import { useSelector } from 'react-redux';
 import { userSelector } from 'containers/Login/reducer';
+import { icons } from 'components/Icons';
 import { useKeycloak } from '@react-keycloak/web';
 
+import Pdf from 'assets/icons/Tentang/pdf-svgrepo-com.svg';
+
 const ContactUs = () => {
+  const { keycloak } = useKeycloak();
   const user = useSelector(userSelector);
   const [errorUploadFile, setErrorUploadFile] = useState('');
   const [data, setData] = useState({
     attachment: [],
   });
   const dispatch = useDispatch();
-  const { keycloak } = useKeycloak();
   const isLoggedIn = !!keycloak.authenticated;
 
   const formControl = async (key, value) => {
@@ -67,11 +73,18 @@ const ContactUs = () => {
 
   const handleUploadFile = async (e) => {
     const file = e?.target?.files?.[0];
+
+    var totalSize = 0;
+    data.attachment.forEach((item) => {
+      totalSize += item.size;
+    });
+
+    totalSize += file.size;
+
     if (!file) {
       setErrorUploadFile('Please select a file!');
       return '';
     }
-    console.log(file.type);
     if (!fileExtention.includes(file.type)) {
       setErrorUploadFile('Please select a file with jpg, png, or pdf extension!');
       return '';
@@ -80,10 +93,16 @@ const ContactUs = () => {
       setErrorUploadFile('File size must be under 5 Mb!');
       return '';
     }
+
+    if (totalSize >= 5000000) {
+      setErrorUploadFile('All File size must be under 5 Mb!');
+      return '';
+    }
+
     setErrorUploadFile('');
     const bodyFormData = new FormData();
     bodyFormData.append('file', file);
-    const uploadedFile = await axios.post(apiUrls.crmImageApi, bodyFormData);
+    const uploadedFile = await axios.post('http://localhost:4040/api/image', bodyFormData);
     if (uploadedFile.status === 200) {
       const attachmentList = data.attachment;
       const newUploadedFile = {
@@ -98,7 +117,13 @@ const ContactUs = () => {
     return '';
   };
 
+  const deleteAttachment = (e) => {
+    data.attachment = data.attachment.filter((item, index) => index !== e);
+    formControl('attachment', data.attachment);
+  };
+
   const handleReset = () => {
+    formControl('attachment', []);
     formRef.current.reset();
   };
 
@@ -147,11 +172,21 @@ const ContactUs = () => {
     <div id="tentang-form" className="contactus-container">
       <div className="contactus-wrapper">
         <div className="contactus-title mb-4">Hubungi Kami</div>
+        {user == null ? (
+          <p>
+            Sudah Miliki Akun? Silahkan&nbsp;
+            <a className="cursor-pointer text-bold" onClick={keycloak.login}>
+              Login
+            </a>
+          </p>
+        ) : (
+          <p></p>
+        )}
         <Form ref={formRef} onSubmit={handleSubmit(onSubmitProses)}>
           <Form.Group controlId="fullName" className="mb-3">
             <Form.Label>Nama Lengkap</Form.Label>
             <Form.Control
-              defaultValue={user?.nama}
+              value={user?.nama}
               isValid={false}
               type="text"
               onChange={(e) => {
@@ -164,7 +199,7 @@ const ContactUs = () => {
           <Form.Group controlId="email" className="mb-3">
             <Form.Label>Email</Form.Label>
             <Form.Control
-              defaultValue={user?.email}
+              value={user?.email}
               type="email"
               onChange={(e) => {
                 setValue('email', e.target.value);
@@ -184,7 +219,7 @@ const ContactUs = () => {
             {errors?.telephone?.message && <div className={'error-message'}>{errors?.telephone?.message}</div>}
           </Form.Group>
           <Form.Group controlId="summary" className="mb-3">
-            <Form.Label>Ringkasan</Form.Label>
+            <Form.Label>Summary</Form.Label>
             <Form.Control
               isValid={false}
               type="text"
@@ -195,7 +230,7 @@ const ContactUs = () => {
             {errors?.summary?.message && <div className={'error-message'}>{errors?.summary?.message}</div>}
           </Form.Group>
           <Form.Group controlId="message" className="mb-3">
-            <Form.Label>Pesan</Form.Label>
+            <Form.Label>Deskripsi</Form.Label>
             <Form.Control
               onChange={(e) => {
                 setValue('message', e.target.value);
@@ -215,13 +250,43 @@ const ContactUs = () => {
                       <span className="text-black font-weight-bold">Browse</span>{' '}
                       <span className="text-muted">Files to Upload</span>
                     </div>
-                    <div className="mt-1 text-muted">Format files must be (*.jpg, *.png, *.pdf ) Maximum 5mb</div>
+                    <div className="mt-1 text-muted">_*Format files must be (*.jpg, *.png, *.pdf ) Maximum 5mb_</div>
                   </div>
                 </div>
               </div>
             </label>
             <Form.Control onChange={handleUploadFile} type="file" hidden></Form.Control>
-            {data.attachment.length !== 0 && <Attachment fileData={data.attachment} />}
+            <Container>
+              <Row>
+                {data.attachment.length !== 0 &&
+                  data.attachment.map((attachmentFile, index) => (
+                    <Col xs lg="4" className="mb-5 mt-5">
+                      <div className="bg-gray rounded-lg" key={`file-${attachmentFile.type}-${index}`}>
+                        <Button
+                          style={{
+                            'border-radius': '50%',
+                            float: 'right',
+                          }}
+                          className="float-right"
+                          type="button"
+                          onClick={() => deleteAttachment(index)}>
+                          <icons.close props={'text-white'} />
+                        </Button>
+                        <div className="flex flex-col justify-center h-full p-1">
+                          {attachmentFile.type === 'application/pdf' ? (
+                            <div>
+                              <img src={Pdf} alt="" />
+                              <div className="text-center text-gray1 mt-3">{attachmentFile.name}</div>
+                            </div>
+                          ) : (
+                            <img className="image" src={attachmentFile.file} alt="file-data" />
+                          )}
+                        </div>
+                      </div>
+                    </Col>
+                  ))}
+              </Row>
+            </Container>
             {errorUploadFile && <div className="my-4 text-xs text-red1">{errorUploadFile}</div>}
           </Form.Group>
           <Button className="w-100" type="submit">

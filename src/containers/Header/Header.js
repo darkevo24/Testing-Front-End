721a49be4ded8fe713c390aea3bbbbde021310ec
Administrator <admin@example.com>
2023-01-05 02:26:30 +0000

    Preprod

--- src/containers/Header/Header.js
+++ src/containers/Header/Header.js
@@ -153,18 +153,18 @@ export const Header = () => {
   const MEMBER_ROUTES = useMemo(
     () => [
       ...COMMON_ROUTES,
-      // {
-      //   title: 'Katalog Data Nasional',
-      //   links: [
-      //     { title: 'Kode Referensi', link: '/#' },
-      //     { title: 'Data Induk', link: '/#' },
-      //     { title: 'Code List', link: '/#' },
-      //     { title: 'Daftar Data', link: '/#' },
-      //     { title: 'Data Browser', link: '/#' },
-      //     { title: 'Manajemen Persetujuan', link: '/#' },
-      //   ],
-      // },
-      // { title: 'Master Data', link: '/#' },
+      {
+        title: 'Katalog Data Nasional',
+        links: [
+          { title: 'Kode Referensi', link: 'http://10.42.0.62:3000/daftar-kode-referensi' },
+          { title: 'Data Induk', link: 'http://10.42.0.62:3000/daftar-data-induk' },
+          { title: 'Code List', link: 'http://10.42.0.62:3000/daftar-kode' },
+          { title: 'Daftar Data', link: 'http://10.42.0.62:3000/daftar-data' },
+          { title: 'Data Browser', link: 'http://10.42.0.62:3000/data-browser' },
+          { title: 'Manajemen Persetujuan', link: 'http://10.42.0.62:3000/manajemen-persetujuan' },
+        ],
+      },
+      { title: 'Master Data', link: 'http://10.42.0.62:3000/katalog-data' },
       {
         title: 'Layanan',
         links: [
